cf75d4939f23d08f4ffdf70fc1a10c5270605bad
Angie-Diaz <diaz.angie.2352@eam.edu.co>
2023-04-28 17:07:56 -0500

    Agregamos sobrero

---
+++ src/practico1/Sombreros.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practico1;
+
+/**
+ *
+ * @author Angie Diaz
+ */
+public class Sombreros {
+  private int talla ;
+  private String name;
+  
+    
+    
+    
+    
+}
